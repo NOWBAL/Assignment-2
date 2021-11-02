ded533bd2e6bad8d1873bcf46a2acfb3ca9289b8
NOWBAL <91430952+NOWBAL@users.noreply.github.com>
2021-11-02 08:01:12 -0700

    Add files via upload
    hjkhu

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+
+
+    public static void main(String[] args) {
+        now now = new now();
+            now.result();
+            System.out.println(now.result);
+    }
+}
---
+++ now.java
@@ -0,0 +1,16 @@
+public class now {
+
+
+ 
+    int a = 100;
+        int b = 1;
+        int result = a + b;
+
+    public void result() {
+    }
+}
+
+  
+
+
+
